ce77f3819aee9fd751a1ce840b999ffcdea37cab
Ryan <ryankupk@gmail.com>
2020-11-30 10:04:41 -0700

    need to call procedure

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import mysql.connector
 import os.path
 from db_tunnel import DatabaseTunnel
+from flask_cors import CORS
 
 DB_HOST = "cs.westminstercollege.edu"
 DB_SSH_PORT = 2322
@@ -15,31 +16,21 @@ DB_USER = "token_2f91"
 DB_PASSWORD = "tfH8abOjPBRtvaCA"
 
 
-try:
-    with \
-        DatabaseTunnel(DB_SSH_KEYFILE) as tunnel:
-            connection = mysql.connector.connect(host='localhost', port=tunnel.getForwardedPort(), database=DB_NAME, user=DB_USER, password=DB_PASSWORD, use_pure=True, autocommit=True)
-            cursor = connection.cursor()
+with \
+    DatabaseTunnel(DB_SSH_KEYFILE) as tunnel:
+        connection = mysql.connector.connect(host='localhost', port=tunnel.getForwardedPort(), database=DB_NAME, user=DB_USER, password=DB_PASSWORD, use_pure=True, autocommit=True)
+        cursor = connection.cursor()
+        
+        app = Flask(__name__)
+        CORS(app)
+        @app.route('/', methods=["GET", "POST"])
+        def index():
+            if request.method == "POST":
+
+                cursor.execute("CALL addToFavorite()")
+            cursor.execute("SELECT Pokemon.number, Pokemon.name, Location.name, Generation.name FROM Pokemon JOIN Location ON Pokemon.location = Location.ID JOIN Generation ON Pokemon.generation = Generation.ID")
+            return render_template('index.html', userDetails=cursor)
             
-            # for (pokemon, name, location, generation) in cursor:
-            #     print(pokemon, name, location, generation)
-            # cursor.close()
-except mysql.connector.Error as err:
-    print(err)
 
-
-
-cursor.execute("SELECT Pokemon.number, Pokemon.name, Location.name, Generation.name FROM Pokemon JOIN Location ON Pokemon.location = Location.ID JOIN Generation ON Pokemon.generation = Generation.ID")
-
-for (pokemon, name, location, generation) in cursor:
-    print(pokemon, name, location, generation)
-# cursor.close()
-
-app = Flask(__name__)
-@app.route('/', methods=["GET", "POST"])
-def index():
-    # pokemon = [[1, "bulbasaur", "cannot be caught", "kanto"]]
-    return render_template('index.html', userDetails=pokemon)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        if __name__ == "__main__":
+            app.run(debug=True)
