a4176dd620250bf2b1f1eeddc74b4cc31504b882
Ryan <ryankupk@gmail.com>
2020-12-01 09:31:42 -0700

    completed but ugly

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, make_response
 import mysql.connector
 import os.path
 from db_tunnel import DatabaseTunnel
@@ -25,15 +25,39 @@ with \
         CORS(app)
         @app.route('/')
         def index():
-            cursor.execute("SELECT Pokemon.number, Pokemon.name, Location.name, Generation.name FROM Pokemon JOIN Location ON Pokemon.location = Location.ID JOIN Generation ON Pokemon.generation = Generation.ID")
-            return render_template('index.html', userDetails=cursor)
+            cursor.execute("SELECT COUNT(*) FROM Pokemon")
+            curNumber = cursor.fetchall()
+            cursor.execute("SELECT COUNT(*) FROM PokemonFavorite")
+            favNumber = cursor.fetchall()
+            cursor.execute("""SELECT Pokemon.number, Pokemon.name, Location.name, Generation.name FROM Pokemon 
+                                JOIN Location ON Pokemon.location = Location.ID 
+                                JOIN Generation ON Pokemon.generation = Generation.ID""")
+            curPokes = cursor.fetchall()
+            cursor.execute("""SELECT PokemonFavorite.number, PokemonFavorite.name, Location.name, Generation.name FROM PokemonFavorite 
+                                JOIN Location ON PokemonFavorite.location = Location.ID 
+                                JOIN Generation ON PokemonFavorite.generation = Generation.ID""")
+            favList = cursor.fetchall()
+            return render_template('index.html', curNumber=curNumber, favNumber=favNumber, curList=curPokes, favList=favList)
 
         @app.route("/favorite", methods=[ "POST" ])
-        def favorite(yeet):
-            pokemon = request.form
-            number = pokemon["number"]
+        def favorite():
+            req = request.get_json()
+            pokemon = jsonify(req)
 
-            cursor.execute("CALL addToFavorite(" + number + ")")
+            res = make_response({}, 200)
+            cursor.execute("CALL FavoritePokemon(" + req["number"] +  ")")
+
+            return res
+
+        @app.route("/unfavorite", methods=[ "POST" ])
+        def unfavorite():
+            req = request.get_json()
+            pokemon = jsonify(req)
+
+            res = make_response({}, 200)
+            cursor.execute("CALL UnfavoritePokemon(" + req["number"] +  ")")
+
+            return res
 
 
             
